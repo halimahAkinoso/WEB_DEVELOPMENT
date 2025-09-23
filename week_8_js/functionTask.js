603e4b87570f1367bcababf8035efaae0fedb84b
halimah Akinoso <halimahakin2@gmail.com>
2025-09-23 16:42:10 +0100

    function task added1

--- week_8_js/functionTask.js
+++ week_8_js/functionTask.js
@@ -95,22 +95,60 @@ if (temperature < 32 && isRaining) {
 
 console.log("");
 
+  let balance = 50000;
 function atm(balance, action, amount){
-    let balance = 50000;
+  
     if (action === "withdraw"){
         if (amount > balance){
             console.log("Insufficient fund")
-            return balance
+            return balance;
+        }else if(amount> 500){
+            console.log("withdrawal limit exceeded maximum withdrawal is 500 at once")
+            return balance;    
+            
         }
+    }else if (action === "deposit"){
+        if (amount <= 0){
+            console.log("Invalid amount");
+            return balance
+        }else {
+            balance +=amount;
+            console.log(`deposite successful, ${balance}`);
+            return balance;
+        }
+    }else{
+        console.log(`Invalid action, make use of withdrwal or deposite`);
+        return balance
     }
+}
 
-
-    console.log("welcome to task 4")
+   
 
 console.log("");
-}
+
 console.log("");
 function personalAss(){
+
+
+
+  let message = day === "workday" ? "Get focused for work!" :
+                day === "weekend" ? "Chill out, it's the weekend!" :
+                day === "holiday" ? "Happy holiday!" : "Invalid day type.";
+  message += (weather === "rainy") ? " Don't forget your umbrella." :
+             (weather === "sunny") ? " Grab some sunscreen if you're heading out." :
+             (weather === "cloudy") ? " Might be a chill day." : " Weather condition not recognized.";
+  if (time < 12 && time >= 0) {
+    message += " Have a great morning!";
+  } else if (time >= 12 && time < 18) {
+    message += " Have a productive afternoon!";
+  } else if (time >= 18 && time <= 23) {
+    message += " Wind down for the evening.";
+  } else {
+    message += " Invalid time.";
+  }
+
+
+
     console.log("welcome to task 5")
     console.log("");
 }
